eb242d448836f53fe7af878f82e76e67c77e1b7b
NSSSSVODF23 <max95202@gmail.com>
2022-10-13 10:48:33 +0300

    cors enable

--- src/main/java/com/microel/speedtest/common/configurations/CorsConfig.java
+++ src/main/java/com/microel/speedtest/common/configurations/CorsConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-//@Configuration
+@Configuration
 public class CorsConfig {
 
 	@Bean
@@ -15,16 +15,16 @@ public class CorsConfig {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 
 		CorsConfiguration config = new CorsConfiguration();
-//		config.setAllowCredentials(true);
-//		config.addAllowedOriginPattern("http://192.168.0.*:*");
-//		config.addAllowedOrigin("http://localhost:4200");
-//		config.addAllowedOrigin("http://10.1.3.150:4200");
-//		config.addAllowedOrigin("http://speedtest.vdonsk.ru");
-//		config.addAllowedOrigin("http://10.50.5.15");
-//		config.addAllowedHeader("*");
-//		config.addAllowedMethod("*");
+		config.setAllowCredentials(true);
+		config.addAllowedOriginPattern("http://192.168.0.*:*");
+		config.addAllowedOrigin("http://localhost:4200");
+		config.addAllowedOrigin("http://10.1.3.150:4200");
+		config.addAllowedOrigin("http://speedtest.vdonsk.ru");
+		config.addAllowedOrigin("http://10.50.5.15");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
 
-//		source.registerCorsConfiguration("/**", config);
+		source.registerCorsConfiguration("/**", config);
 		// source.registerCorsConfiguration("/graphql", config);
 
 		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
